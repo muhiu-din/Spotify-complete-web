16d006990cf760704072b05f773a2d857f3a81cb
Muhiu Din <muhiudin005@gmail.com>
2025-02-11 00:48:20 +0500

    made playlist

---
+++ script.js
@@ -0,0 +1,16 @@
+function addBox(image ,heading ,text) {
+    let doc = document.body.getElementsByClassName(box);
+    add
+                <div class="innerlists">
+                    <div class="box">
+                        <div class="image ">
+                            <img src="${image}" alt="dp">
+                        </div>
+                        <div class="text">
+                            <h1>"${heading}"</h1>
+                            <p>"${text}"</p>
+                        </div>
+                    </div>
+                </div>
+}
+addBox("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiiNSZIpfMO6mIbPHTR9lGR8H1SIN4wvAaiQ&s" , "Song Cover" , "Let the beat hit niggas")
